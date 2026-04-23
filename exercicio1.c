3e5ecaa15e03223f7d92c3346bfeb3d4839740db
juan75indiano@gmail.com <juan75indiano@gmail.com>
2026-04-22 22:13:07 -0300

    first commit

---
+++ exercicio1.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <time.h>
+
+#define PRODUTOS 10
+
+int main(){
+    int estoque[PRODUTOS];
+    int opcao, produto, quantidade;
+
+    srand(time(NULL));
+
+    
+}
